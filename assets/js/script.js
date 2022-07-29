b45791beafdc662103948458b7b16e2823c94808
Nathan Mayhook <jnmayhook@gmail.com>
2022-07-28 22:06:21 -0700

    updated local storage, save button working

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var hours = ["9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 var millitaryTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var currentHour = moment().hour();
 var userText = $("textarea");
-
+userText = [""];
 // handle displaying the time
 function displayTime() {
   var currentDay = moment().format("dddd, MMMM Do");
@@ -14,21 +14,10 @@ function displayTime() {
 displayTime();
 
 function createPlannerDisplay() {
-  //     var rowEl = document.createElement("<row>")
-  //     var eventHourEl = document.createElement("<col-1>").addClass("hour").text(hour);
-  //     var eventDetailEL = document.createElement("<col-10>").addClass("event").text(event);
-  //     var eventSubmitEl = document.createElement("<col-1>").addClass("event button").text(submit);
-
-  // rowEl.append(
-  //     eventHourEl,
-  //     eventDetailEL,
-  //     eventSubmitEl
-  // );
-
-  // scheduleDisplayEl.append(rowEl);
   var timeBlock = "";
 
   for (var i = 0; i < hours.length; i++) {
+    
     var statusTime = "";
     if (currentHour === millitaryTime[i]) {
       statusTime = "present";
@@ -41,31 +30,24 @@ function createPlannerDisplay() {
       timeBlock +
       `<div class="row">
     <div class="col-1 hour text-right">${hours[i]}</div>
-    <div class="col-10 event"><textarea class="form-control ${statusTime}" name="" id="" cols="30" rows="3"></textarea></div>
-    <div class="col-1 submit text-left"><button class="saveBtn">Save</button></div>
+    <div class="col-10 event"><textarea class="form-control ${statusTime}" name="" id="${i}" cols="30" rows="3"></textarea></div>
+    <div class="col-1 submit text-left"><button class="saveBtn" data-id="${i}">Save</button></div>
     </div>`;
   }
   scheduleDisplayEl.innerHTML = timeBlock;
   var saveBtn = document.querySelectorAll(".saveBtn");
   for (var i = 0; i < saveBtn.length; i++) {
+    var textBox = document.getElementById(i);
+    if (localStorage.getItem(i)) {
+      textBox.value = localStorage.getItem(i);
+    }
     saveBtn[i].addEventListener("click", save);
   }
 }
 function save() {
-    JSON.parse.localStorage.setItem(hours[i], userText);
+  var id = this.getAttribute("data-id")
+  var textBox = document.getElementById(id)
+
+  localStorage.setItem(id, textBox.value)
 }
-
 createPlannerDisplay();
-
-// Timeblocks are color coded: events in the past are gray, current hour is red, new event is green
-
-// When a user clicks into the timeblock they can enter an event
-// eventInputEl.on('click', function () {
-
-// });
-
-// When a user clicks the save button, the event is stored into local storage
-
-// When page is refreshed saved event persists
-
-// on cliick local
